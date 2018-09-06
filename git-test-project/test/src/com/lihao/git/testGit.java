08166458d4cbc596f75e1f5062c9b8d061b81079
lihao <l914937433@126.com>
2018-09-06 15:33:04 +0800

    第二次改动了

--- git-test-project/test/src/com/lihao/git/testGit.java
+++ git-test-project/test/src/com/lihao/git/testGit.java
@@ -11,11 +11,13 @@ public class testGit {
         System.out.println("hello git");
 
         System.out.println("修改了");
+        System.out.println("第二次修改了");
     }
 
     public void test(){
 
         int i = 0;
+        int a = 1;
 
     }
 
