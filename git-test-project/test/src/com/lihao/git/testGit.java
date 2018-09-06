500e2f49c8faefea30c21936589d9a148bcd063d
lihao <l914937433@126.com>
2018-09-06 14:11:14 +0800

    第一次提交

---
+++ git-test-project/test/src/com/lihao/git/testGit.java
@@ -0,0 +1,13 @@
+package com.lihao.git;
+
+public class testGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
